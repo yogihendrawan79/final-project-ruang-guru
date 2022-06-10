000eea633cf94035541f188cb9a7aede698d0dd3
elroypedrokameo <infinitygamexray@gmail.com>
2022-06-10 23:30:04 +0700

    feat: pagination

--- Frontend/src/components/Soal/CardSoal/Card.jsx
+++ Frontend/src/components/Soal/CardSoal/Card.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
-import Button from '../Button/Button'
+// import Button from '../Button/Button'
 import './card.css'
 
-function Card({soal, option}) {
-  console.log(option)
+const Card = ({soals}) => {
+
   return (
     <>
       <div className="card-soal border-2 border-primary px-5 py-5">
-        <div>
-          <p className='flex justify-start'>{soal}</p>
-        </div>
-        <div className="opsi flex-row mt-2 cursor-default">
-          <ul>
-            <li className='option inline-grid'>
-              <a href='/#'>{option.A}</a>
-              <a href='/#'>{option.B}</a>
-              <a href='/#'>{option.C}</a>
-              <a href='/#'>{option.D}</a>
-            </li>
-          </ul>
-        </div>
-        <Button />
+        {soals.map((soals) => (
+          <div key={soals.id}>
+            <div>
+              <p>{soals.question}</p>
+            </div>
+            <div className="opsi flex-row mt-2 cursor-default">
+            {/* {soals.option.map((opsi) => (
+              <p key={opsi}>{opsi}</p>
+            ))} */}
+              <p className='option inline-grid'>{soals.question.option}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   )
--- Frontend/src/components/Soal/Soal.jsx
+++ Frontend/src/components/Soal/Soal.jsx
@@ -1,40 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Navbar from '../Navbar/Navbar'
 import Card from './CardSoal/Card'
 import Daftar from './DaftarSoal/Daftar'
 
 
 function Soal() {
+  const [soals, setSoals] = useState([]);
+  const [currentPage, setCurrentPage] = useState(false)
+  const [soalsPerPage] = useState(1)
+
   const data = [
     {
       image: "https://images.unsplash.com/photo-1495995424756-6a5a3f9e7543?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nzl8fHN0dWRlbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-      countdown: "20:90",
+      countdown: "02:00:00",
       username: "John"
     }
   ]
 
-  const soal = [
-    {
-      id: "1",
-      question: "23. Indonesia merdeka pada tahun?",
-      option: {
-        A: "A. 1945",
-        B: "B. 1999",
-        C: "C. 1928",
-        D: "D. 1960"
-      }
-    },
-    // {
-    //   id: "2",
-    //   question: "23. Siapakah presiden RI yang pertama?",
-    //   option: {
-    //     A: "A. Ir. Soekarno",
-    //     B: "B. Soeharto",
-    //     C: "C. Joko Widodo",
-    //     D: "D. Naruto"
-    //   }
-    // },
-  ]
+  useEffect(() => {
+    const fetchSoals = async () => {
+      const res = await axios.get('http://localhost:8000/soal')
+      setSoals(res.data)
+    }
+
+    fetchSoals();
+  }, [])
+  // console.log("Hasil fetch", soals)
+
+  // Get current soal
+  const indexOfLastSoal = currentPage * soalsPerPage;
+  const indexOfFirstSoal = indexOfLastSoal - soalsPerPage;
+  const currentSoals = soals.slice(indexOfFirstSoal, indexOfLastSoal)
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
   return (
     <>
@@ -44,10 +44,12 @@ function Soal() {
        ))
      }
      <div className='flex justify-center mt-10'>
-      {soal.map((soal) => (
-       <Card key={soal.id} soal={soal.question} option={soal.option} />
-      ))}
-       <Daftar />
+      <Card soals={currentSoals} />
+      <Daftar
+        soalsPerPage={soalsPerPage}
+        totalSoals={soals.length}
+        paginate={paginate}
+      />
      </div>
     </>
   )
