ecc1b9f7174c6db19f1341736ccfe847f795cd1e
elroypedrokameo <infinitygamexray@gmail.com>
2022-06-12 06:43:03 +0700

    feat: kondisi lulus/tidak lulus di result page

--- Frontend/src/components/Result/Result.jsx
+++ Frontend/src/components/Result/Result.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Navbar/Navbar'
 import Image from '../../assets/images.jpg'
 import Button from '../Soal/Button/Button'
 import './result.css'
 
 const Result = () => {
+  const [status] = useState('Lulus')
   return (
     <> 
       <Navbar logo=".ET" username="John" image={Image} />
@@ -13,10 +14,22 @@ const Result = () => {
           <h1 className='mt-12 font-bold text-5xl'>80</h1>
           <p>Nilai anda</p>
         </div>
-        <h1 className='font-bold text-center mt-2 text-2xl text-lulus'>Selamat</h1>
-        <div className='mt-2'>
-          <p className='text-center'>Anda telah lulus dalam mata pelajaran matematika</p>
-        </div>
+        {status === "Lulus" 
+          ? 
+            <div>
+              <h1 className='font-bold text-center mt-2 text-2xl text-lulus'>Selamat</h1>
+              <div className='mt-2'>
+                <p className='text-center'>Anda telah lulus dalam mata pelajaran matematika</p>
+              </div>
+            </div>
+          :
+            <div>
+              <h1 className='font-bold text-center mt-2 text-2xl text-gagal'>Mohon maaf!</h1>
+              <div className='mt-2'>
+                <p className='text-center'>Anda tidak lulus dalam mata pelajaran matematika</p>
+              </div>
+            </div>
+        }
         <div className='button flex justify-center mt-4'>
           <Button button="Selesai" />
         </div>
