c1d0a0ef3c10db56aa7736b0016ff39730fa8493
Yogi Hendrawan <hendrawanyogi79@gmail.com>
2022-06-13 13:52:08 +0700

    fix: fixing card material

--- Frontend/src/components/Dashboard/BankSoal/BankSoal.jsx
+++ Frontend/src/components/Dashboard/BankSoal/BankSoal.jsx
@@ -15,44 +15,26 @@ const BankSoal = () => {
     {
       id: "1",
       mapel: "Matematika",
-      durasi: "30",
-      token: "asdfjaslkj",
-      deadline: "2020-01-01",
     },
     {
       id: "2",
       mapel: "IPA",
-      durasi: "30",
-      token: "ksjkjsdkjf",
-      deadline: "2020-01-01",
     },
     {
       id: "3",
       mapel: "IPS",
-      durasi: "30",
-      token: "jksjkdfjk",
-      deadline: "2020-01-01",
     },
     {
       id: "4",
       mapel: "Fisika",
-      durasi: "30",
-      token: "jksdfjkfj",
-      deadline: "2020-01-01",
     },
     {
       id: "5",
       mapel: "Kimia",
-      durasi: "30",
-      token: "awisksksk",
-      deadline: "2020-01-01",
     },
     {
       id: "6",
       mapel: "B. Indonesia",
-      durasi: "30",
-      token: "lakjsdfkfj",
-      deadline: "2020-01-01",
     },
   ];
 
@@ -69,13 +51,7 @@ const BankSoal = () => {
       <div className="flex justify-start">
         <div className="grid grid-cols-5">
           {bank.map((index) => (
-            <Card
-              key={index.id}
-              mapel={index.mapel}
-              durasi={index.durasi}
-              token={index.token}
-              deadline={index.deadline}
-            />
+            <Card key={index.id} mapel={index.mapel} />
           ))}
         </div>
       </div>
--- Frontend/src/components/Dashboard/BankSoal/CardBank/Card.jsx
+++ Frontend/src/components/Dashboard/BankSoal/CardBank/Card.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 
-function Card({ mapel, durasi, token, deadline }) {
+function Card({ mapel }) {
   return (
     <>
       <div className="flex justify-start ml-20 mt-10">
         <a
           href="#"
-          class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          class="block p-6 w-52 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 class="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
             {mapel}
           </h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">
-            Durasi: {durasi} Menit
-          </p>
-          <p class="font-normal text-gray-700 dark:text-gray-400">
-            Token: {token}
-          </p>
-          <p class="font-normal text-gray-700 dark:text-gray-400">
-            Deadline: {deadline}
-          </p>
         </a>
       </div>
     </>
