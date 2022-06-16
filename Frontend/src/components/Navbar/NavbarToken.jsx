eca4a36b8b9965392f91f73fa0cd41d272004871
Yogi Hendrawan <hendrawanyogi79@gmail.com>
2022-06-16 14:12:45 +0700

    fix: fixing navbartoken

--- Frontend/src/components/Navbar/NavbarToken.jsx
+++ Frontend/src/components/Navbar/NavbarToken.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 // import downarrow from '../../assets/down-arrow.png'
-
 
 const NavbarToken = ({ logo, username, image }) => {
   return (
     <>
       <div className="container-lg bg-primary">
         <div className="container flex justify-between py-5">
-          <div className='navbar-brand pl-28'>
-            <Link to={'#/'} className='text-2xl text-white font-bold'>{logo}</Link>
+          <div className="navbar-brand pl-28">
+            <Link to={"#/"} className="text-2xl text-white font-bold">
+              {logo}
+            </Link>
           </div>
-          <div className='flex justify-center align-middle'>
-            <p className='pr-3 text-white'>Hi, {username}</p>
-            <img src={image} alt="profile" className='profile' />
+          <div className="flex justify-center align-middle">
+            <p className="pr-3 text-white">Hi, {username}</p>
+            <img src={image} alt="profile" className="profile mr-5" />
             {/* <img src={downarrow} alt="" className='' /> */}
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default NavbarToken
+export default NavbarToken;
